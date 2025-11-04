016eb9c5e2840d8a1f19cc364569de16c375dc7c
Christian <cdduran84@gmail.com>
2025-11-03 21:11:58 -0500

    quitar contactos

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 export default function App() {
